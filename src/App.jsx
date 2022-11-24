12fe3e31ce4a19958720fe04548a8c4ca93b3e4b
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-11-24 23:30:45 +0500

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      {/* <SimpleSlider/> */}
-      {/* <Component/> */}
+    
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+const Footer = ()=>{
+    return(
+        <div>
+            salom
+        </div>
+    )
+}
+
+
+export default Footer;
